fdf35742eec9f88154c1e237c1e896216fa75bb3
Jeffrey_T <tian1303@mydusd.org>
2020-05-29 23:59:32 -0700

    postgre use

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from db import db
 
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -10,7 +11,7 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #prevents flask-sqlalchemy from tracking changes to the database
                                                     # regular SQLAlchemy does it instead
 app.secret_key = 'jeff'
